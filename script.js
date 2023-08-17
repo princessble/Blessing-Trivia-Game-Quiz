2174e30e196e96edf1525f9a93522017d36ab7e1
princessble <kwomofortuneb@gmail.com>
2023-08-17 18:06:10 +0100

    Implement interactive quiz functionality and user experience enhancements in JavaScript

--- script.js
+++ script.js
@@ -1,80 +1,166 @@
-// Sample quiz questions and answers
-const quizQuestions = [
-    {
-        question: "What is the capital of France?",
-        options: ["Paris", "London", "Berlin", "Madrid"],
-        correctAnswer: "Paris"
-    },
-    {
-        question: "Which planet is known as the 'Red Planet'?",
-        options: ["Venus", "Mars", "Jupiter", "Saturn"],
-        correctAnswer: "Mars"
-    },
-    // Add more questions here...
-];
+// Get the necessary HTML elements
+const usernameContainer = document.getElementById("username-container");
+const startButton = document.getElementById("start-button");
+const questionElement = document.getElementById("question");
+const optionsElement = document.getElementById("options");
+const scoreElement = document.getElementById("score-value");
+const feedbackElement = document.getElementById("feedback");
 
-// Initialize variables
+// Initialize variables for tracking quiz progress and score
 let currentQuestionIndex = 0;
 let score = 0;
 
-// Function to display a new question
-function displayQuestion() {
-    const questionElement = document.getElementById("question");
-    const optionsElement = document.getElementById("options");
-    const feedbackElement = document.getElementById("feedback");
-
-    questionElement.textContent = quizQuestions[currentQuestionIndex].question;
-    optionsElement.innerHTML = "";
-
-    quizQuestions[currentQuestionIndex].options.forEach((option, index) => {
-        const optionButton = document.createElement("button");
-        optionButton.classList.add("option");
-        optionButton.textContent = option;
-
-        optionButton.addEventListener("click", () => {
-            checkAnswer(option);
-        });
-
-        optionsElement.appendChild(optionButton);
-    });
-
-    feedbackElement.textContent = "";
-}
-
-// Function to check user's answer
-function checkAnswer(selectedOption) {
-    const feedbackElement = document.getElementById("feedback");
-
-    if (selectedOption === quizQuestions[currentQuestionIndex].correctAnswer) {
-        feedbackElement.textContent = "Correct!";
-        score++;
-    } else {
-        feedbackElement.textContent = "Incorrect. The correct answer was: " + quizQuestions[currentQuestionIndex].correctAnswer;
-    }
-
-    currentQuestionIndex++;
-
-    if (currentQuestionIndex < quizQuestions.length) {
+// Function to start the quiz
+function startQuiz() {
+    const username = document.getElementById("username").value;
+    if (username.trim() !== "") {
+        // Hide the username input container
+        usernameContainer.style.display = "none";
+        
+        // Display the first question
         displayQuestion();
     } else {
-        displayResult();
+        // Display an error message or prompt the user to enter a valid username
     }
 }
 
-// Function to display the final result
-function displayResult() {
-    const questionElement = document.getElementById("question");
-    const optionsElement = document.getElementById("options");
-    const feedbackElement = document.getElementById("feedback");
-    const scoreElement = document.getElementById("score");
+// Event listener for the start button
+startButton.addEventListener("click", startQuiz);
 
-    questionElement.textContent = "Quiz completed!";
+// Array of quiz questions
+const questions = [
+    // Question objects with options and correct answers
+    // Add more questions here
+        {
+            question: "1. What is the capital of France?",
+            options: ["Paris", "London", "Berlin", "Madrid"],
+            correctAnswer: 0
+        },
+        {
+            question: "2. Which planet is known as the 'Red Planet'?",
+            options: ["Venus", "Mars", "Jupiter", "Saturn"],
+            correctAnswer: 1
+        },
+        {
+            question: "3. Which famous scientist developed the theory of general relativity?",
+            options: ["Isaac Newton", "Albert Einstein", "Galileo Galilei", "Stephen Hawking"],
+            correctAnswer: 1
+        },
+        {
+            question: "4. What is the largest mammal on Earth?",
+            options: ["Blue Whale", "Elephant", "Giraffe", "Lion"],
+            correctAnswer: 0
+        },
+        {
+            question: "5. Which country is known as the 'Land of the Rising Sun'?",
+            options: ["China", "Japan", "South Korea", "Thailand"],
+            correctAnswer: 1
+        },
+        {
+            question: "6. Which famous artist painted the Mona Lisa?",
+            options: ["Vincent van Gogh", "Leonardo da Vinci", "Pablo Picasso", "Michelangelo"],
+            correctAnswer: 1
+        },
+        {
+            question: "7. What is the chemical symbol for gold?",
+            options: ["Au", "Ag", "G", "Go"],
+            correctAnswer: 0
+        },
+        {
+            question: "8. Which famous playwright wrote 'Romeo and Juliet'?",
+            options: ["William Shakespeare", "George Orwell", "Charles Dickens", "Jane Austen"],
+            correctAnswer: 0
+        },
+        {
+            question: "9. What is the largest planet in our solar system?",
+            options: ["Venus", "Mars", "Jupiter", "Saturn"],
+            correctAnswer: 2
+        },
+        {
+            question: "10. Which animal is known as the 'King of the Jungle'?",
+            options: ["Lion", "Tiger", "Elephant", "Giraffe"],
+            correctAnswer: 0
+        },
+
+        {
+            question: "11. Which gas do plants use for photosynthesis?",
+            options: ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen"],
+            correctAnswer: 1
+        },
+        {
+            question: "12. In which year did the Titanic sink?",
+            options: ["1905", "1912", "1921", "1930"],
+            correctAnswer: 1
+        },
+        {
+            question: "13. What is the largest species of shark?",
+            options: ["Hammerhead Shark", "Great White Shark", "Tiger Shark", "Bull Shark"],
+            correctAnswer: 1
+        },
+        {
+            question: "14. Which planet is known as the 'Morning Star' or 'Evening Star'?",
+            options: ["Venus", "Mars", "Mercury", "Saturn"],
+            correctAnswer: 0
+        },
+        {
+            question: "15. Who wrote the novel 'Pride and Prejudice'?",
+            options: ["Jane Austen", "Emily Brontë", "Charlotte Brontë", "Agatha Christie"],
+            correctAnswer: 0
+        },
+    ];
+    
+
+// Function to display a question
+function displayQuestion() {
+    const currentQuestion = questions[currentQuestionIndex];
+    
+    // Display the current question
+    questionElement.textContent = currentQuestion.question;
+    
+    // Display answer options as buttons
     optionsElement.innerHTML = "";
-    feedbackElement.textContent = "Your final score: " + score + " out of " + quizQuestions.length;
-    scoreElement.textContent = "Score: " + score;
+    currentQuestion.options.forEach((option, index) => {
+        const button = document.createElement("button");
+        button.textContent = option;
+        button.addEventListener("click", () => checkAnswer(index));
+        optionsElement.appendChild(button);
+    });
 }
 
-// Load the first question when the page loads
-window.addEventListener("load", () => {
-    displayQuestion();
-});
+// Function to check the selected answer
+function checkAnswer(selectedIndex) {
+    const currentQuestion = questions[currentQuestionIndex];
+    if (selectedIndex === currentQuestion.correctAnswer) {
+        score++;
+        feedbackElement.textContent = "Correct!";
+    } else {
+        feedbackElement.textContent = "Incorrect!";
+    }
+    
+    // Update score and move to the next question
+    scoreElement.textContent = score;
+    currentQuestionIndex++;
+    
+    // Check if there are more questions to display
+    if (currentQuestionIndex < questions.length) {
+        displayQuestion();
+    } else {
+        // Display quiz completion message
+        questionElement.textContent = "Quiz Completed!";
+        optionsElement.innerHTML = "";
+    }
+}
+
+// Function to submit the quiz
+function submitQuiz() {
+    // Calculate final score, display results, etc.
+    // Customize this function based on your requirements
+    // For now, let's display an alert with the final score
+    alert(`Quiz completed!\nYour final score is: ${score}/${questions.length}`);
+}
+
+// Event listener for the "Submit Quiz" button
+document.getElementById("submit-button").addEventListener("click", submitQuiz);
+
+// Display the first question when the page loads
+displayQuestion();
