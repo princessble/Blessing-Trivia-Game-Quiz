975fd1faf8f586c0494e653fcb6c01c57784279a
Blessing Fortune <kwomofortuneb@gmail.com>
2024-01-14 14:15:26 +0000

    Add final score

--- assets/js/script.js
+++ assets/js/script.js
@@ -147,7 +147,7 @@ document.addEventListener("DOMContentLoaded", function () {
    * For now, let's display the final score
    */
   function submitQuiz() {
-    finalScoreElementContainer.classList.remove("none");
+    finalScoreElementContainer.classList.remove("hide");
     finalScoreElement.innerHTML = score;
     submitButton.style.display = "none";
     restartButton.style.display = "block";
